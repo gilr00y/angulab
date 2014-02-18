3c41f27e3bdf8f103ef56994d3211930e62a512d
Jason Gilbertson <jgilbertson@customink.com>
2014-02-17 20:08:22 -0500

    Tiny bit of styling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,31 @@ var images = [
   {src:'images/SVG/drop.svg'},
   {src:'images/SVG/label.svg'},
   {src:'images/SVG/packman.svg'},
-  {src:'images/SVG/skull.svg'}
+  {src:'images/SVG/skull.svg'},
+  {src: 'images/SVG/backpack.svg'},
+  {src: 'images/SVG/chalkboard.svg'},
+  {src: 'images/SVG/eye.svg'},
+  {src: 'images/SVG/map.svg'},
+  {src: 'images/SVG/paper plane.svg'},
+  {src: 'images/SVG/speakers.svg'},
+  {src: 'images/SVG/bill.svg'},
+  {src: 'images/SVG/clock.svg'},
+  {src: 'images/SVG/flag.svg'},
+  {src: 'images/SVG/map2.svg'},
+  {src: 'images/SVG/photo 2.svg'},
+  {src: 'images/SVG/store.svg'},
+  {src: 'images/SVG/bookmark.svg'},
+  {src: 'images/SVG/cloud check.svg'},
+  {src: 'images/SVG/glasses.svg'},
+  {src: 'images/SVG/marker.svg'},
+  {src: 'images/SVG/piggy.svg'},
+  {src: 'images/SVG/tactic.svg'},
+  {src: 'images/SVG/bookshelf.svg'},
+  {src: 'images/SVG/cloud down.svg'},
+  {src: 'images/SVG/glove.svg'},
+  {src: 'images/SVG/mcfly.svg'},
+  {src: 'images/SVG/pin.svg'},
+  {src: 'images/SVG/toaster.svg'}
 ];
 
 angular.module('angulabApp')
--- app/scripts/directives/main.js
+++ app/scripts/directives/main.js
@@ -5,9 +5,9 @@ angular.module('angulabApp')
     return {
       restrict: 'A',
       link: function(arg1, arg2, arg3) {
-        console.log("1: " + arg1);
-        console.log("2: " + arg2);
-        console.log("3: " + arg3);
+        console.log('1: ' + arg1);
+        console.log('2: ' + arg2);
+        console.log('3: ' + arg3);
       }
     };
   });
