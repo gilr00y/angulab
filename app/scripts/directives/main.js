4ea2a3aa623419325f790797c1689ce69c570db1
Jason Gilbertson <jgilbertson@customink.com>
2014-02-18 22:57:56 -0500

    Use SVG.draggable for draggable elements; Plugin doens't work with nested SVGs, would have to modify

--- app/scripts/directives/main.js
+++ app/scripts/directives/main.js
@@ -2,43 +2,33 @@
 
 var SVG = window.SVG;
 
-// these should be moved into a service
-function wrapSVG(element) {
-  return SVG(element);
-}
-
-function moveSVG() {
-  var element = wrapSVG(this);
-  element.dmove(20, 20);
-}
-
-function serializeSVG(svg) {
-  return new XMLSerializer().serializeToString(svg);
-}
-
 angular.module('angulabApp')
   .directive('designable', ['$compile', function($compile) {
     return {
       restrict: 'A',
       scope: { name: '=' },
       link: function(scope, element) {
-        // new DesignElement(element);
 
         element.on('click', function() {
           var designElement = angular.element(element[0].innerHTML);
-          designElement.attr('clicked', true);
+          designElement.attr('draggable', true);
 
           angular.element(window.Design.node).prepend($compile(designElement)(scope));
-          designElement.on('click', moveSVG);
         });
       }
     };
   }])
-  .directive('clicker', function() {
+  .directive('draggable', function() {
     return {
       restrict: 'A',
-      link: function(scope, element) {
-        element.on('click', function() { console.log('SECONDARY CLICKED'); });
+      link: {
+        pre: function(scope, element) {
+          //something here
+        },
+        post: function(scope, element) {
+          SVG(element[0]).draggable();
+        }
       }
+
     };
   });
