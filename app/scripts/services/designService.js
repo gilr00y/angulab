dfa30f7a2492419f88dc2bf317a6e8152fb3a7dd
Jason Gilbertson <jgilbertson@customink.com>
2014-03-13 08:49:47 -0400

    Refactor design factory into service

--- app/scripts/services/designService.js
+++ app/scripts/services/designService.js
@@ -1,34 +1,37 @@
 angular.module('angulabApp')
-  .factory('designService', function() {
-    var design;
-    var elements = {};
+  .service('designService', Design);
 
-    return {
-      addElement: function(element, id) {
-        elements[id] = element;
-      },
-      allElements: function() {
-        return elements;
-      },
-      setAttr: function(id, attr, value) {
-        if(typeof elements[id] !== 'undefined') {
-          elements[id][attr] = value;
-        }
-        else {
-          throw new Error('Element with id: ' + id + ' does not exist');
-        }
-      },
-      getAttr: function(id, attr) {
-        if(typeof elements[id] !== 'undefined') {
-          return elements[id][attr];
-        }
-        else {
-          throw new Error('Element with id: ' + id + ' does not exist');
-        }
-      },
-      getElement: function(id) {
-        return elements[id];
-      },
-      design: design
-    }
-  });
+function Design() {
+  this.design;
+  this.elements = {};
+}
+
+Design.prototype.addElement = function(element, id) {
+  this.elements[id] = element;
+};
+
+Design.prototype.allElements = function() {
+  return this.elements;
+};
+
+Design.prototype.setAttr = function(id, attr, value) {
+  if(typeof this.elements[id] !== 'undefined') {
+    this.elements[id][attr] = value;
+  }
+  else {
+    throw new Error('Element with id: ' + id + ' does not exist');
+  }
+};
+
+Design.prototype.getAttr = function(id, attr) {
+  if(typeof this.elements[id] !== 'undefined') {
+    return this.elements[id][attr];
+  }
+  else {
+    throw new Error('Element with id: ' + id + ' does not exist');
+  }
+};
+
+Design.prototype.getElement = function(id) {
+  return this.elements[id];
+};
